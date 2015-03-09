61890f5d9e45a8dfc8e4df3fc5f55b3dd3146869
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-09 16:51:59 -0700

    Simplify to have a simple Node/Express server with User and Place models. Work on serving up a simple index.html page.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var handler = require('./lib/request-handler');
-
 var app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', handler.renderIndex);
-
-app.get('/login', handler.loginUserForm);
-app.post('/login', handler.loginUser);
-app.get('/logout', handler.logoutUser);
-
-app.get('/signup', handler.signupUserForm);
-app.post('/signup', handler.signupUser);
 
 module.exports = app;
