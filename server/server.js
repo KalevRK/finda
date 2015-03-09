73004eab3e75e62a07b758d6cebf8701cc7b2ada
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-09 13:26:45 -0700

    Server is up and running.

--- server/server.js
+++ server/server.js
@@ -3,8 +3,12 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/mvp');
 
-module.exports.app = express();
+var app = express();
+
+module.exports.app = app;
 
 app.get('/', function (req, res) {
   res.send('This is a server response.');
 });
+
+app.listen(5309);
