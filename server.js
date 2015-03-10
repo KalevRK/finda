a81213076f5992026d6c1a45f9f1e1f7e97e695a
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-03-09 19:40:35 -0700

    Implement a basic RESTful server using Node, Express, MongoDB, and Mongoose.

---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/mvp');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+app.listen(5309);
+console.log('Listening on port 5309');
+
+// Mongoose model
+var Place = mongoose.model('Place', {
+    name: String
+});
+
+// routes
+
+// GET all places
+app.get('/api/places', function(req, res) {
+    Place.find(function(err, places) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(places);
+    });
+});
+
+// POST a new place
+app.post('/api/places', function(req, res) {
+  Place.create({
+    name: req.body.name
+  }, function (err, place) {
+    if (err) {
+        res.send(err);
+    }
+    
+    Place.find(function(err, places) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(places);
+    });
+
+  });
+});
+
+// DELETE a place
+app.delete('/api/places/:place_id', function(req, res){
+  Place.remove({
+    _id: req.params.place_id
+  }, function(err, place) {
+    if (err) {
+        res.send(err);
+    }
+
+    Place.find(function(err, places) {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(places);
+    });
+  });
+});
